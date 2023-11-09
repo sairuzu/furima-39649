fc830bdad658aea02035ec913aacb9d82eabceae
sairuzu <l4_1960st@yahoo.co.jp>
2023-11-09 21:59:23 +0900

    テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {'test'}
+    email                 {'test@example'}
+    password              {'000aaa'}
+    password_confirmation {password}
   end
 end
