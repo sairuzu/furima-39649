bbec06e2e904bcfa1abcf7728a6e18cfabec4986
sairuzu <l4_1960st@yahoo.co.jp>
2023-11-11 00:06:05 +0900

    テストコード（修正）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,6 +38,13 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
+    it '重複したemailが存在する場合は登録できない' do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
+
     it 'emailは@を含まないと登録できない' do
       @user.email = 'testmail'
       @user.valid?
