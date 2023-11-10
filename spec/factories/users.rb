f88a250ca522e2632c942e377e3eaf08a9dd0e2f
sairuzu <l4_1960st@yahoo.co.jp>
2023-11-11 00:06:33 +0900

    テストコード（修正）

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
     email                 {'test@example'}
     password              {'000aaa'}
     password_confirmation {password}
+    last_name             {'焼肉'}
+    first_name            {'太郎'}
+    last_name_kana        {'ヤキニク'}
+    first_name_kana       {'タロウ'}
+    birth_date            {'19990101'}
   end
 end
