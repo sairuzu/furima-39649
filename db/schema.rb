d0397f5c4545a92a216701c8837a4214d92f0d4f
sairuzu <l4_1960st@yahoo.co.jp>
2023-11-18 20:45:41 +0900

    schema修正

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_12_113446) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["price"], name: "index_items_on_price"
+    # t.index ["price"], name: "index_items_on_price"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
