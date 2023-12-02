595d29f45dcc7e7d4ffde1f5a62d131ef2888278
sairuzu <l4_1960st@yahoo.co.jp>
2023-12-02 18:24:05 +0900

    購入機能の実装　修正②

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -3,21 +3,14 @@ class OrderShipping
   attr_accessor :post_code, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number,
                 :item_id, :user_id, :token
   
-  validates :post_code, presence: true,
-                        format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-  
-  validates :prefecture_id, presence: true, exclusion: { in: ['---'] }
-  
-  validates :municipalities, :street_address, presence: true
-
-  with_options presence: true, format: { with: /\A[0-9]+\z/, message:  'is invalid. Please enter numbers only' }, length: { in: 10..11 } do
-    validates :telephone_number
-  end
-
-  with_options presence: true  do
-   validates :user_id
-   validates :item_id
-   validates :token
+  with_options presence: true do
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, exclusion: { in: ['---'] }
+    validates :municipalities, :street_address
+    validates :telephone_number, format: { with: /\A[0-9]+\z/, message: 'is invalid. Please enter numbers only' }, length: { in: 10..11 }
+    validates :user_id
+    validates :item_id
+    validates :token
   end
 
   def save
